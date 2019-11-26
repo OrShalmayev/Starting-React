61b201801d9a49c2ef3a898397970d8ce4a8c744
unknown <shalmayev1993@gmail.com>
2019-11-26 22:18:30 +0200

    added example 3 (counter)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; // let us render the app to our page
 import './index.css'; // thats will tell webpack to pull this css file and put it on the page
+
+/*********************************Example 1 */
+
 // Components
 // function Custom(){
 //     return <span>I'm a Custom component.</span>;
@@ -44,37 +47,105 @@ import './index.css'; // thats will tell webpack to pull this css file and put i
 //         </>
 //     );
 // }
-const IconButton = ({children}) => (
-        <button>
-            <i className="fas fa-cloud" /> {children}
-        </button>
-);
-const Alert = ({children, title}) => (
-    <div className="alert">
-       <i className="fas fa-exclamation-triangle"></i>
-       <h3>{title}</h3>
-       {children}
-    </div>
-);
+// ReactDOM.render(
+//     <App />, 
+//     document.querySelector('#root')
+// );
 
-const App = () => (
-    <>
-        {/* Render dynamic content */}
-        <IconButton>Awesome</IconButton>
-        <Alert
-            title={
-                <>
-                    <p>Testing</p>
-                    <em>Hey! Listen!</em>
-                </>
-            }>
-            Very <em>Important</em> message!
-        </Alert>
-    </>
-);
 
-// Selecting element to put the component detail in it
-ReactDOM.render(
-    <App />, 
-    document.querySelector('#root')
-);
+/*********************************Example 2 */
+// const IconButton = ({children}) => (
+//         <button>
+//             <i className="fas fa-cloud" /> {children}
+//         </button>
+// );
+// const Alert = ({children, title}) => (
+//     <div className="alert">
+//        <i className="fas fa-exclamation-triangle"></i>
+//        <h3>{title}</h3>
+//        {children}
+//     </div>
+// );
+
+// const App = () => (
+//     <>
+//         {/* Render dynamic content */}
+//         <IconButton>Awesome</IconButton>
+//         <Alert
+//             title={
+//                 <>
+//                     <p>Testing</p>
+//                     <em>Hey! Listen!</em>
+//                 </>
+//             }>
+//             Very <em>Important</em> message!
+//         </Alert>
+//     </>
+// );
+
+// // Selecting element to put the component detail in it
+// ReactDOM.render(
+//     <App />, 
+//     document.querySelector('#root')
+// );
+
+
+
+
+/*********************************Example 3 */
+class Counter extends React.Component{
+    
+    state = {
+        count: 5
+    };
+
+    constructor(props){
+        super(props);
+        console.log(props);
+        // this.state = {
+        //     count:25
+        // }
+
+    }
+
+    increment = () => {
+        this.setState({
+            count: this.state.count + 1
+        }, ()=>console.log(this.state.count));
+    }
+
+    decrement = () => {
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+
+    render(){
+        return (
+            <div className="counter">
+                <h2>Counter</h2>
+                <div>
+                    <button onClick={this.decrement}>-</button>
+                    <span className="count">{this.state.count}</span>
+                    <button onClick={this.increment}>+</button>
+                </div>
+            </div>
+        );
+    }
+
+
+}
+// const Counter = () => (
+//     <>
+//         <div className="counter">
+//             <h2>Counter</h2>
+//             <div>
+//                 <button>-</button>
+//                 <span className="count">0</span>
+//                 <button>+</button>
+//             </div>
+//         </div>
+//     </>
+// );
+
+ReactDOM.render(<Counter/>, document.querySelector('#root'))
